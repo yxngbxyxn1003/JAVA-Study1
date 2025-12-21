a1cb36c29362795a218ddd52cf48708e0bbcee40
Yang-Boyun <boyunyang@naver.com>
2025-12-21 14:15:40 +0900

    feat : init project execept variable

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,5 +1,1 @@
-public class HelloJava{ //Hello.Java 클래스의 범위 시작
-    public static void main (String[] args){
-        System.out.println("hello java");
-    }
-}
+
--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -1,7 +1,1 @@
-public class HelloJava2{
-     public static void main(String[] args) {
-         System.out.println("hello Java1");
-         System.out.println("hello Java2");
-         System.out.println("hello java3");
-     }
-}
+
