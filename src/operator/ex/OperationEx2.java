7207cb5ae4b73473940ba1b2b5e031a6d9c82398
Yang-Boyun <boyunyang@naver.com>
2026-01-03 01:24:41 +0900

    feat : operation solution completed

--- src/operator/ex/OperationEx2.java
+++ src/operator/ex/OperationEx2.java
@@ -1,1 +1,19 @@
+package operator.ex;
 
+public class OperationEx2 {
+    public static void main(String[] args) {
+
+        double val1 = 1.5;
+        double val2 = 2.5;
+        double val3 = 3.5;
+
+        double sum;
+        double average;
+
+        sum = val1 + val2 + val3;
+        average = (val1 + val2 + val3) / 3;
+
+        System.out.print(sum);
+        System.out.print(average);
+    }
+}
