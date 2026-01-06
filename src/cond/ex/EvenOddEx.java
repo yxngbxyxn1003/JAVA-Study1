e1f556c208c9df690f7ba8acd0c184d64ae2603b
Yang-Boyun <boyunyang@naver.com>
2026-01-06 17:12:46 +0900

    feat : 조건문 문제풀이

--- src/cond/ex/EvenOddEx.java
+++ src/cond/ex/EvenOddEx.java
@@ -1,1 +1,19 @@
+package cond.ex;
 
+/**
+ * 문제: 홀수 짝수 찾기
+ * 정수 x가 주어지면 x가 짝수일 경우 "짝수"를 , x가 홀수일 경우 "홀수"를 출력하는 프로그램을 작성하라.
+ * 삼항 연산자를 사용할 것
+ */
+
+public class EvenOddEx {
+
+    public static void main(String[] args) {
+        int x = 20;
+
+
+        String type = (x % 2 == 0) ? "짝수" : "홀수" ;
+
+        System.out.println("x = " + x + " , " + type);
+    }
+}
--- src/cond/ex/MoveRateEx.java
+++ src/cond/ex/MoveRateEx.java
@@ -1,1 +1,33 @@
+package cond.ex;
+/**
+ * 문제: "평점에 따른 영화 추천하기"
+ * 요청한 평점 이상의 영화를 찾아서 추천하는 프로그램을 작성하자.
 
+ 어바웃타임 - 평점9
+ 토이 스토리 - 평점8
+ 고질라 - 평점7
+
+ 평점 변수는 double rating을 사용하세요. if 문을 활용해서 문제를 풀자.
+
+ 출력 예시
+ rating:9
+ '어바웃타임'을 추천합니다.
+ */
+
+public class MoveRateEx {
+    public static void main(String[] args) {
+        double rating = 8;
+
+        System.out.println("rating = " + rating);
+
+        if(rating >= 9){
+            System.out.println("어바웃 타임을 추천합니다.");
+        }
+        if (rating >= 8) {
+            System.out.println("토이 스토리를 추천합니다.");
+        }
+        if (rating >= 7) {
+            System.out.println("고질라를 추천합니다.");
+        }
+    }
+}
