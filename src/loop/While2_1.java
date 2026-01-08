7707d5fb1680403ee1f9295ec0c90cc224100f2d
Yang-Boyun <boyunyang@naver.com>
2026-01-08 15:07:55 +0900

    feat : while

--- src/loop/While2_1.java
+++ src/loop/While2_1.java
@@ -1,1 +1,20 @@
+package loop;
 
+/** 문제 : 1부터 하나씩 증가하는 수를 3번 더해라 (1 ~ 3 더하기)
+ * 이 문제는 1부터 하나씩 증가하는 수이기 때문에 1 + 2 + 3을 더해야 한다.
+   우선 while 문을 사용하지 않고 단순하게 작성해볼 것
+ */
+public class While2_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        sum = sum + 1;
+        System.out.println("i = " + 1 + " sum=" + sum);
+
+        sum = sum + 2;
+        System.out.println("i = " + 1 + " sum=" + sum);
+
+        sum = sum + 3;
+        System.out.println("i = " + 1 + " sum=" + sum);
+    }
+}
--- src/loop/While2_2.java
+++ src/loop/While2_2.java
@@ -1,1 +1,38 @@
+package loop;
 
+/**
+public class While2_2 {
+    public static void main(String[] args) {
+        // i부터 하나씩 증가하는 수를 3번 더해라
+
+        int i = 1;
+        int sum = 0;
+
+        while(i < 4) {
+            sum = sum + i;
+            System.out.println("i : " + i + " sum = " + sum);
+            i++;
+        }
+    }
+}
+**/
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 10;
+
+        sum = sum + i;
+        System.out.println("i= " + i + " sum = " + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i= " + i + "sum = " + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i= " + i + "sum = " + sum);
+        i++;
+
+    }
+}
