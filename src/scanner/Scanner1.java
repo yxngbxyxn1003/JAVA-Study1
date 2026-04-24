fcc47df08493afba18b5105ad6fda8a57fb26346
Yang-Boyun <boyunyang@naver.com>
2026-04-24 18:15:30 +0900

    Scanner1.java

--- src/scanner/Scanner1.java
+++ src/scanner/Scanner1.java
@@ -1,1 +1,17 @@
+package scanner;
 
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자열을 입력하세요. :  ");
+        String str = scanner.nextLine();
+        System.out.println("입력한 문자열 : " + str);
+
+        System.out.println("정수를 입력하세요. : ");
+        int Intvalue  = scanner.nextInt();
+        System.out.println("입력한 숫자 : " + Intvalue);
+    }
+}
