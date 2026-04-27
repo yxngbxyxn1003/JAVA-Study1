6b2a439f516cb06bb8a80da2f45bf5b83951b829
Yang-Boyun <boyunyang@naver.com>
2026-04-27 15:43:51 +0900

    ScannerWhile1

--- src/scanner/ScannerWhile1.java
+++ src/scanner/ScannerWhile1.java
@@ -1,1 +1,19 @@
+package scanner;
 
+import java.util.Scanner;
+
+public class ScannerWhile1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("문자열을 입력하세요(exit : 종료) : ");
+            String str = scanner.nextLine();
+            if(str.equals("exit")) {
+                System.out.print("프로그램을 종료합니다. ");
+                break;
+            }
+            System.out.print("입력한 문자열: " + str);
+        }
+    }
+}
