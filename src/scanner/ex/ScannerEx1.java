e9b119f700f26920506d3cd4aa0e503fc57c5ed8
Yang-Boyun <boyunyang@naver.com>
2026-04-29 16:43:50 +0900

    ScannerEx2

--- src/scanner/ex/ScannerEx1.java
+++ src/scanner/ex/ScannerEx1.java
@@ -1,1 +1,19 @@
+package scanner.ex;
 
+import java.util.Scanner;
+
+public class ScannerEx1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요 : ");
+
+        int num = input.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.print("입력한 숫자 " + num + "는 짝수입니다.");
+        } else System.out.print("입력한 숫자 " + num + "는 홀수입니다.");
+
+    }
+}
