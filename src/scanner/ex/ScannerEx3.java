214ab2e261ee466d10fe26152b0e47b21ef75d70
Yang-Boyun <boyunyang@naver.com>
2026-04-29 17:35:35 +0900

    구구단 출력

--- src/scanner/ex/ScannerEx3.java
+++ src/scanner/ex/ScannerEx3.java
@@ -1,1 +1,20 @@
+package scanner.ex;
 
+import java.util.Scanner;
+
+public class ScannerEx3 {
+    public static void main(String[] args) {
+
+        Scanner multiplication = new Scanner(System.in);
+
+        System.out.println("구구단의 단 수를 입력해주세요. : ");
+
+        int num = multiplication.nextInt();
+
+        System.out.println(num +"단의 구구단 : ");
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(num + "X" + i + "=" + num * i);
+        }
+    }
+}
