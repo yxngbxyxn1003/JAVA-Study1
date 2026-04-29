6d642698ac6fcdbd147465b7ee44098ff060cd21
Yang-Boyun <boyunyang@naver.com>
2026-04-29 17:47:32 +0900

    변수 값 교환

--- src/scanner/ex/ScannerEx4.java
+++ src/scanner/ex/ScannerEx4.java
@@ -1,1 +1,16 @@
+package scanner.ex;
 
+public class ScannerEx4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
