c334acf40f66a18b8597edb8f1f1f03aafd0b39a
Yang-Boyun <boyunyang@naver.com>
2026-05-18 15:05:23 +0900

    변수만 사용해서 학생 점수 출력

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -1,1 +1,17 @@
+package array;
 
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
+
+        System.out.println("학생 1 점수 "+ student1 );
+        System.out.println("학생 2 점수 "+ student2 );
+        System.out.println("학생 3 점수 "+ student3 );
+        System.out.println("학생 4 점수 "+ student4 );
+        System.out.println("학생 5 점수 "+ student5 );
+    }
+}
