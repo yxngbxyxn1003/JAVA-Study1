6fc33f3e1929a4a001af680179342f73ea4213e2
Yang-Boyun <boyunyang@naver.com>
2026-05-18 15:42:49 +0900

    메모리 주소 참조값 출력 코드 추가

--- src/array/Array1Ref1.java
+++ src/array/Array1Ref1.java
@@ -5,6 +5,8 @@ public class Array1Ref1 {
         int[] students; // 배열 변수 선언
         students = new int[5];
 
+        System.out.println(students);
+
         // 변수 값 대입
         students[0] = 90;
         students[1] = 80;
