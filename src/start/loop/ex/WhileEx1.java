0399752ac179d41bb8e5d3e0fffafbc823a05a82
Yang-Boyun <boyunyang@naver.com>
2026-06-23 15:48:04 +0900

    Add while loop to print natural numbers from 1 to 10

--- src/start/loop/ex/WhileEx1.java
+++ src/start/loop/ex/WhileEx1.java
@@ -1,1 +1,15 @@
+package loop.ex;
 
+public class WhileEx1 {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while(count < 10) {
+            count++;
+            System.out.println(count);
+
+            // 아니면 초기값을 1로 잡고 count++를 출력문 앞에 써도 됨
+        }
+    }
+}
